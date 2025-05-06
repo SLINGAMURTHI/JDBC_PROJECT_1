9d7d905dbee906250f148983106954345d3b8829
S B LINGAMURTHI <sblingamurty@gmail.com>
2025-05-06 09:31:53 +0530

    Update Program1.java

--- src/com/Test/Program1.java
+++ src/com/Test/Program1.java
@@ -5,6 +5,12 @@ public class Program1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		Scanner sc=new Scanner(System.in);
+		//Square of a number.
+		System.out.print("Enter a Number");
+		int n=sc.nextInt();
+		System.out.println("Square of "+ n +" is: "+(n*n));
+	
 
 	}
 
